a98ff8992b8e4b953902b22918a3577e02ce2b0b
Your Name <applezafar@gmail.com>
2022-05-17 22:35:09 +1000

    Homework4

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.util.concurrent.LinkedTransferQueue;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         for (int i = 1; i <= 10; i++)
             System.out.print(i + " ");
-        System.out.println();
+        System.out.println();{}
 
         int a = 11;
         while (a > 1) {
             a = a - 1;
-            System.out.print(a + " ");
+            System.out.print(a + " ");}
 
+            int friday = 6;
+        {
+            for (int i = 6; i<=31; i = i+7)
+            System.out.println("Today is Friday" + i + "We need to prepare a report");}
 
-            }
-}
-}
+    }}
