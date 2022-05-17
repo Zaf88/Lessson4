fa6032a9efa0776f6744078e66ea23eac6e6f96c
Your Name <applezafar@gmail.com>
2022-05-17 20:41:35 +1000

    Homework4

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+            System.out.print(i + " ");
+        System.out.println();
+
+        int a = 11;
+        while (a > 1) {
+            a = a - 1;
+            System.out.print(a + " ");
+
+
+            }
+}
 }
