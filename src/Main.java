ec734117702e03a35b305852bfa2ac65ecd24161
Your Name <applezafar@gmail.com>
2022-05-19 14:43:26 +1000

    Homework4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
+import java.security.PublicKey;
 import java.util.concurrent.LinkedTransferQueue;
 
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++)
             System.out.print(i + " ");
-        System.out.println();{}
+        System.out.println();
+        {
+        }
 
         int a = 11;
         while (a > 1) {
             a = a - 1;
-            System.out.print(a + " ");}
+            System.out.print(a + " ");
+        }
 
-            int friday = 6;
-        {
-            for (int i = 6; i<=31; i = i+7)
+        int friday = 6;
+
+    {
+        for (int i = 6; i <= 31; i = i + 7)
             System.out.println("Today is Friday" + i + "We need to prepare a report");}
 
-    }}
+
+        int presentYear = 2021;
+        int startYear = presentYear -200-4;
+        int endYear = presentYear +100;
+        int startPeriod = 0;
+            for (int i = 1817; i<=2121; i = i +79){
+
+                System.out.println("year when comets fly over the earth" + i);
+            }}}
